dd8dce9efd13b08357f6991b41fd15a35d5c421f
rrothey <rrothey@example.com>
2026-02-19 21:42:38 -0500

    Save changes from workspace (auto)

---
+++ basics/test1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+middlename= input("Enter your middle name: ")
+lastname = input("Enter your last name: ") 
+print("Hello, " + name + " " + middlename + " " + lastname + "!")   
